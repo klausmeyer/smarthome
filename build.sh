c123925bb86a74d672d5f911aa7a6395d8f0d2cb
Klaus Meyer <spam@klaus-meyer.net>
2020-12-22 23:13:45 +0100

    rename temperature to metrics_temperature

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ do
   echo "- Image: $image "
   echo "--------------------------------------------------------------------------------"
 
+  image=$(echo "$image" | tr "_" "-")
+
   docker build -t 127.0.0.1:5000/smarthome/$image images/$image
   docker push 127.0.0.1:5000/smarthome/$image
 done
